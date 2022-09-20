e2071920a40288955b879eb606ca346f04f275fc
MariaL <visdy@mail.ru>
2022-09-20 18:04:32 +0300

    begin

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Итоговое задание 1 четверть
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecArray(string[] array1,string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i, array1.Length; i++)
+    {
+        if (array1 [i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
